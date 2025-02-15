b86ada33772b2d352580349cc22254e0b2176d42
arathy.nair <arathy.nair@youlend.com>
2025-02-15 12:29:45 +0000

    fixed: releasebranchdiff: output

--- outputs.tf
+++ outputs.tf
@@ -327,7 +327,7 @@ output "private_route_table_association_ids" {
 
 output "database_route_table_association_ids" {
   description = "List of IDs of the database route table association"
-  value       = values(aws_route_table_association.database)[*].id
+  value       = aws_route_table_association.database[*].id
 
 }
 
