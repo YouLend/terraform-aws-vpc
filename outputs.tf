3bb187407094971c27b58a9daeb742dcadbff7eb
arathy.nair <arathy.nair@youlend.com>
2025-02-17 18:51:07 +0000

    fix: testing19791: updated output

--- outputs.tf
+++ outputs.tf
@@ -358,10 +358,11 @@ output "nat_ids" {
   value       = values(aws_eip.nat)[*].id
 }
 
-output "nat_public_ips" {
+ output "nat_public_ips" {
   description = "List of public Elastic IPs created for AWS NAT Gateway"
-    value       = var.reuse_nat_ips ? var.external_nat_ips : values(aws_eip.nat)[*].public_ip
+  value       = var.reuse_nat_ips ? var.external_nat_ips : [for k, eip in aws_eip.nat : eip.public_ip if contains(keys(aws_nat_gateway.this), k)]
 }
+
 
 
 output "natgw_ids" {
