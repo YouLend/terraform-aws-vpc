f7598939a1e089b8933f0ceda183d49283cad261
arathy.nair <arathy.nair@youlend.com>
2025-02-25 12:12:18 +0000

    updated: testing19791: subnet

--- main.tf
+++ main.tf
@@ -483,30 +483,35 @@ resource "aws_subnet" "public_eks_green" {
 #################
 # Private subnet
 #################
-
 resource "aws_subnet" "private" {
-  for_each = { for idx, cidr in var.private_subnets : idx => cidr }
+  count = var.create_vpc && length(var.private_subnets) > 0 ? length(var.private_subnets) : 0
 
   vpc_id                          = local.vpc_id
-  cidr_block                      = each.value
-  availability_zone               = length(regexall("^[a-z]{2}-", element(var.azs, tonumber(each.key)))) > 0 ? element(var.azs, tonumber(each.key)) : null
-  availability_zone_id            = length(regexall("^[a-z]{2}-", element(var.azs, tonumber(each.key)))) == 0 ? element(var.azs, tonumber(each.key)) : null
+  cidr_block                      = var.private_subnets[count.index]
+  availability_zone               = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) > 0 ? element(var.azs, count.index) : null
+  availability_zone_id            = length(regexall("^[a-z]{2}-", element(var.azs, count.index))) == 0 ? element(var.azs, count.index) : null
   assign_ipv6_address_on_creation = var.private_subnet_assign_ipv6_address_on_creation == null ? var.assign_ipv6_address_on_creation : var.private_subnet_assign_ipv6_address_on_creation
 
-  ipv6_cidr_block = var.enable_ipv6 && length(var.private_subnet_ipv6_prefixes) > 0 ? cidrsubnet(aws_vpc.this[0].ipv6_cidr_block, 8, var.private_subnet_ipv6_prefixes[tonumber(each.key)]) : null
+  ipv6_cidr_block = var.enable_ipv6 && length(var.private_subnet_ipv6_prefixes) > 0 ? cidrsubnet(aws_vpc.this[0].ipv6_cidr_block, 8, var.private_subnet_ipv6_prefixes[count.index]) : null
 
   tags = merge(
     {
       "Name" = format(
         "%s-${var.private_subnet_suffix}-%s",
         var.name,
-        element(var.azs, tonumber(each.key)),
+        element(var.azs, count.index),
       )
     },
     var.tags,
     var.private_subnet_tags,
   )
+  lifecycle {
+    ignore_changes = [
+      tags["Supported_Environment"]
+    ]
+  }
 }
+
 
 ##################
 ###############################################################################
